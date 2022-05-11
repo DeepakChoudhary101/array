539d54e64254893f41e62cf935549a490c1a8e03
DeepakChoudhary101 <dpkchoudhary103@gmail.com>
2022-05-11 22:56:55 +0530

    cheakeven

---
+++ evenbyarray.java
@@ -0,0 +1,16 @@
+class EvenByArray
+{
+    public static void main(String args[])
+    {
+      int a[]={2,3,4,5,6,7,8,9,0};
+       {
+          for(int i=0;i<a.length;i++)
+           {
+              if(a[i]%2==0)
+               {
+                 System.out.println("even no is "+a[i]);
+               }
+           }
+       }
+   }
+}
